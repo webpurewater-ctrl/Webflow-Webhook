3d348e89b73454bf0bf5d0c770d87e8d02810b55
purewater <web.purewater@gmail.com>
2026-06-29 11:11:27 -0400

    fixed time out issues

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
-const config = require("./config");
-const app = require("./app");
-
-app.listen(config.port, () => {
-  console.log(`Webhook server listening on port ${config.port}`);
-});
+const app = require("../src/app");
+module.exports = app;
