b299e6450eaa70902449a526134a1b395a6d8dfc
purewater <web.purewater@gmail.com>
2026-06-29 15:50:17 -0400

    vercel node setup

--- api/index.js
+++ api/index.js
@@ -1,4 +1,20 @@
+const express = require("express");
 const serverless = require("serverless-http");
-const app = require("../src/app");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Hello from Vercel!"
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "UP",
+    timestamp: new Date().toISOString()
+  });
+});
 
 module.exports = serverless(app);
